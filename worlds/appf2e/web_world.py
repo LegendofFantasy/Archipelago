20d435c297a60d0e089178c5937aad65a347f170
LegendofFantasy <235428721+LegendofFantasy@users.noreply.github.com>
2025-11-15 05:24:48 -0500

    Remove old comments

--- worlds/appf2e/web_world.py
+++ worlds/appf2e/web_world.py
@@ -20,7 +20,4 @@ class APPF2eWebWorld(WebWorld):
     )
 
     tutorials = [setup_en]
-
-    # If we have option groups and/or option presets, we need to specify these here as well.
     option_groups = option_groups
-    #options_presets = option_presets
