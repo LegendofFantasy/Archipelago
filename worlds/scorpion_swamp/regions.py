e911460ba978356e15a0a167f5db6335a1f3e9cd
LegendofFantasy <235428721+LegendofFantasy@users.noreply.github.com>
2026-01-27 13:42:50 -0500

    Update regions.py
    Minor edit to remove a no longer used import

--- worlds/scorpion_swamp/regions.py
+++ worlds/scorpion_swamp/regions.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from BaseClasses import Entrance, Region
+from BaseClasses import Region
 
 if TYPE_CHECKING:
     from .world import ScorpionSwampWorld
